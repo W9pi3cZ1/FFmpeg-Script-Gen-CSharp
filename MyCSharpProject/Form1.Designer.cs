16f315891321b5e104eccb7118f538e0880e6333
XSlimeNB <116885083+W9pi3cZ1@users.noreply.github.com>
2023-04-13 22:09:19 +0800

    Add files via upload

---
+++ MyCSharpProject/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace MyCSharpProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(216, 30);
+            label1.TabIndex = 0;
+            label1.Text = "XSlime's C# Project";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Title = "Hey！That is a file dialog.";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 508);
+            button1.Name = "button1";
+            button1.Size = new Size(701, 32);
+            button1.TabIndex = 1;
+            button1.Text = "Open File";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += ButtonClicked;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 92);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(701, 410);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 20);
+            label2.TabIndex = 3;
+            label2.Text = "参数:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(61, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(652, 23);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "{i}. {path} {floder} {enter}";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft YaHei UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 69);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 20);
+            label3.TabIndex = 5;
+            label3.Text = "输出:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(725, 552);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "XSlime's C# Project openFileTest";
+            Load += Form1_Load;
+            Resize += Form1_Resize;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            string path;
+            string command = textBox2.Text;
+            string temp = command;
+            string floder;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = "";
+                for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+                {
+                    floder = System.IO.Path.GetDirectoryName(openFileDialog1.FileNames[i]);
+                    path = openFileDialog1.FileNames[i];  //获取文件路径
+                    temp = command;
+                    temp = temp.Replace("{i}", (i + 1).ToString());
+                    temp = temp.Replace("{path}", path);
+                    temp = temp.Replace("{floder}", floder);
+                    temp = temp.Replace("{enter}", Environment.NewLine);
+                    //textBox1.Text += $"{i + 1}. {path}{Environment.NewLine}";
+                    textBox1.Text += temp;
+                }
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.LastIndexOf(Environment.NewLine), Environment.NewLine.Length);
+            }
+        }
+
+
+        #endregion
+
+        private Label label1;
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+    }
+}
---
+++ MyCSharpProject/Form1.cs
@@ -0,0 +1,67 @@
+namespace MyCSharpProject
+{
+    public partial class Form1 : Form
+    {
+        private float X;
+        private float Y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)//循环窗体中的控件
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                    setTag(con);
+            }
+        }
+
+        /// <summary>
+        /// 根据窗体大小调整控件大小
+        /// </summary>
+        /// <param name="newx">窗体宽度缩放比例</param>
+        /// <param name="newy">窗体高度缩放比例</param>
+        /// <param name="cons">随窗体改变控件大小</param>
+        private void setControls(float newx, float newy, Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls)
+            {
+
+                string[] mytag = con.Tag.ToString().Split(new char[] { ':' });//获取控件的Tag属性值，并分割后存储字符串数组
+                float a = System.Convert.ToSingle(mytag[0]) * newx;//根据窗体缩放比例确定控件的值，宽度
+                con.Width = (int)a;//宽度
+                a = System.Convert.ToSingle(mytag[1]) * newy;//高度
+                con.Height = (int)(a);
+                a = System.Convert.ToSingle(mytag[2]) * newx;//左边距离
+                con.Left = (int)(a);
+                a = System.Convert.ToSingle(mytag[3]) * newy;//上边缘距离
+                con.Top = (int)(a);
+                Single currentSize = System.Convert.ToSingle(mytag[4]) * newy;//字体大小
+                con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                if (con.Controls.Count > 0)
+                {
+                    setControls(newx, newy, con);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            X = this.Width;//获取窗体的宽度
+            Y = this.Height;//获取窗体的高度
+            setTag(this);//调用方法
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            float newx = (this.Width) / X; //窗体宽度缩放比例
+            float newy = (this.Height) / Y;//窗体高度缩放比例
+            setControls(newx, newy, this);//随窗体改变控件大小
+        }
+    }
+}
